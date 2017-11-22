d2f8b2a992db39d61611ff877f6b6f0dd88ba7d4
Matthew Buckner <bucknermr@gmail.com>
2017-11-22 08:49:55 -0800

    Add buttons, content, and styling for hero

--- Gemfile
+++ Gemfile
@@ -33,7 +33,6 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'jquery-rails'
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- frontend/components/header/hero.jsx
+++ frontend/components/header/hero.jsx
@@ -3,6 +3,18 @@ import React from 'react';
 const Hero = () => (
   <div className="hero-container">
     <h1 className="hero-logo"></h1>
+    <div className="hero-buttons-container">
+      <button className="hero-sign-in">Sign in</button>
+      <button className="hero-create-account">Create account</button>
+    </div>
+    <div className="hero-content-container">
+      <h2 className="wtf">Connect on SoundCloud</h2>
+      <p>
+        Discover, stream, and share a constantly expanding mix of music
+        from emerging and major artists around the world.
+      </p>
+      <button>Sign up for free</button>
+    </div>
   </div>
 );
 
