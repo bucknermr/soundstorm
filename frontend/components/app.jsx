5e3acfeb67bc0b383b1d828f8cf8781e2fab0cc2
Matthew Buckner <bucknermr@gmail.com>
2017-11-21 22:30:44 -0800

    Add css reset; add hero background image

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavbarContainer from './header/navbar_container';
 import SessionFormContainer from './session/session_form_container';
+import Hero from './header/hero';
 
 const App = () => {
   return (
     <div>
       <header>
+        <Hero />
         <NavbarContainer />
         <SessionFormContainer />
       </header>
---
+++ frontend/components/header/hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Hero = () => (
+  <div className="hero-container">
+    <h1 className="hero-logo"></h1>
+  </div>
+);
+
+export default Hero;
