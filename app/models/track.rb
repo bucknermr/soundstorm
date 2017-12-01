49692cb53e086ac7acd75c43ef2cf5eac269b70f
Matthew Buckner <bucknermr@gmail.com>
2017-12-01 15:02:19 -0800

    Add support for m4a audio files

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,7 +2,7 @@ class Track < ApplicationRecord
   validates :title, presence: true
   has_attached_file :audio
   validates :audio, attachment_presence: true
-  validates_attachment_file_name :audio, matches: [/mp3\z/, /wav\z/]
+  validates_attachment_file_name :audio, matches: [/mp3\z/, /wav\z/, /m4a\z/]
   has_attached_file :image
   validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
 
